c21b58049fbf29db8b99fd97e51311f948226e90
Dimosthenis Kontogiorgos <diko@kth.se>
2017-11-20 04:22:28 +0100

    Updates realtime.

--- realtime/js/main.js
+++ realtime/js/main.js
@@ -8,6 +8,12 @@ var initScene, render, renderer, scene, camera;
 var bulbLight, bulbMat, hemiLight, object;
 var clock = new THREE.Clock();
 
+// Text definition
+var text1 = document.createElement('div');
+
+var csvStream = null;
+var csvData = null;
+
 // Object definition
 // Glasses
 var g1marker1 = null;
@@ -19,6 +25,18 @@ var g1markerc = null;
 // Gp3
 var gp3marker1 = null;
 
+// Targets
+var t1marker1 = null;
+var t1marker2 = null;
+var t1marker3 = null;
+var t1marker4 = null;
+var t1markerc = null;
+var t2marker1 = null;
+var t2marker2 = null;
+var t2marker3 = null;
+var t2marker4 = null;
+var t2markerc = null;
+
 // Floor
 var floorMat;
 
@@ -26,7 +44,7 @@ var floorMat;
 var running = true;
 var timetostart = startvis;
 var previousShadowMap = false;
-var framesPerSecond = 60; // max 60
+var framesPerSecond = 50; // max 60
 var stats;
 
 // Tobii Glasses 1 helper
@@ -34,7 +52,7 @@ var g1m1_pos, g1m2_pos, g1m3_pos, g1m4_pos;
 var g1mc_pos;
 var g1mc_x, g1mc_y, g1mc_z;
 
-// Glasses Marker Lines
+// Glasses 1 Marker Lines
 var glinegeo1;
 var g1line;
 
@@ -44,6 +62,24 @@ var gp3_pos1;
 // GP3 Marker Lines
 var gp3linegeo1;
 var gp3line1;
+
+// Target 1 helper
+var t1m1_pos, t1m2_pos, t1m3_pos, t1m4_pos;
+var t1mc_pos;
+var t1mc_x, t1mc_y, t1mc_z;
+
+// Target 1 Marker Lines
+var tlinegeo1;
+var t1line;
+
+// Target 2 helper
+var t2m1_pos, t2m2_pos, t2m3_pos, t2m4_pos;
+var t2mc_pos;
+var t2mc_x, t2mc_y, t2mc_z;
+
+// Target 2 Marker Lines
+var tlinegeo2;
+var t2line;
 
 // Light settings
 var bulbLuminousPowers = {
@@ -87,84 +123,131 @@ function onWindowResize() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
-// Read position data from csv file
-function parsePositionData(dataLocation, callBack) {
-    Papa.parse(dataLocation, {
-        download: true,
-        dynamicTyping: true,
-        delimeter: "",
-        complete: function(results) {
-            // When data is parsed
-            console.log("Position data parsed!")
-            callBack(results.data);
-        }
-    });
-}
-
-var csvData;
-function positionDataHandler(data) {
-    // CSV Position Data is usable here
-    csvData = data;
-}
-
-// Read asr data from csv file
-function parseASRData(dataLocation, callBack) {
-    Papa.parse(dataLocation, {
-        download: true,
-        dynamicTyping: true,
-        delimeter: "",
-        complete: function(results) {
-            // When data is parsed
-            console.log("ASR data parsed!")
-            callBack(results.data);
-        }
-    });
-}
-
-var csvASRData;
-function asrDataHandler(data) {
-    // CSV ASR Data is usable here
-    csvASRData = data;
-}
-
-// Read app data from csv file
-function parseAppData(dataLocation, callBack) {
-    Papa.parse(dataLocation, {
-        download: true,
-        dynamicTyping: true,
-        delimeter: "",
-        complete: function(results) {
-            // When data is parsed
-            console.log("App data parsed!")
-            callBack(results.data);
-        }
-    });
-}
-
-var csvAppData;
-function appDataHandler(data) {
-    // CSV App Data is usable here
-    csvAppData = data;
-}
-
-// Read furniture data from csv file
-function parseFurnitureData(dataLocation, callBack) {
-    Papa.parse(dataLocation, {
-        download: true,
-        dynamicTyping: true,
-        delimeter: "",
-        complete: function(results) {
-            // When data is parsed
-            console.log("Furniture data parsed!")
-            callBack(results.data);
-        }
-    });
-}
-
-var csvFurnitureData;
-function furnitureDataHandler(data) {
-    // CSV Furniture Data is usable here
-    csvFurnitureData = data;
-}
+// Connect to web socket
+var ws = new WebSocket('ws://130.237.67.120:8080/');
+ws.onopen = function() {
+    console.log('CONNECT');
+};
+ws.onclose = function() {
+    console.log('DISCONNECT');
+    running = false;
+};
 
 window.onload = initScene();
+
+// var data3d = {
+//    "mocap_target1":{
+//       "name":"target1",
+//       "marker4":{
+//          "y":0.8121280074119568,
+//          "x":-3.0052330493927,
+//          "z":1.2687002420425415
+//       },
+//       "frame":1243884,
+//       "marker1":{
+//          "y":0.8138397932052612,
+//          "x":-3.0820324420928955,
+//          "z":1.3705765008926392
+//       },
+//       "marker3":{
+//          "y":0.8125203847885132,
+//          "x":-3.0178568363189697,
+//          "z":1.370245099067688
+//       },
+//       "marker2":{
+//          "y":0.8139135241508484,
+//          "x":-3.081951856613159,
+//          "z":1.2542681694030762
+//       },
+//       "mocaptimestamp":34603.1719921,
+//       "position":{
+//          "y":0.8131003379821777,
+//          "x":-3.0467684268951416,
+//          "z":1.3159475326538086
+//       },
+//       "rotation":{
+//          "y":2.3824650270398706e-06,
+//          "x":-1.0701196515583433e-05,
+//          "z":1.9223429262638092e-05,
+//          "w":-1.0
+//       },
+//       "id":2,
+//       "localtime":1511138724.6466603
+//    },
+//    "mocap_glasses1":{
+//       "name":"glasses1",
+//       "marker4":{
+//          "y":0.9546578526496887,
+//          "x":-4.62416934967041,
+//          "z":2.4623360633850098
+//       },
+//       "frame":1243884,
+//       "marker1":{
+//          "y":0.9499883651733398,
+//          "x":-4.5850605964660645,
+//          "z":2.518827438354492
+//       },
+//       "marker3":{
+//          "y":0.9483512043952942,
+//          "x":-4.766242980957031,
+//          "z":2.518057346343994
+//       },
+//       "marker2":{
+//          "y":0.9416577219963074,
+//          "x":-4.7567338943481445,
+//          "z":2.5682265758514404
+//       },
+//       "mocaptimestamp":34603.1719921,
+//       "position":{
+//          "y":0.9486637711524963,
+//          "x":-4.683051586151123,
+//          "z":2.516861915588379
+//       },
+//       "rotation":{
+//          "y":-0.8100244402885437,
+//          "x":0.03251512348651886,
+//          "z":0.01980029232800007,
+//          "w":-0.5851590037345886
+//       },
+//       "id":1,
+//       "localtime":1511138724.6466603
+//    },
+//    "mocap_target2":{
+//       "name":"target2",
+//       "marker4":{
+//          "y":0.5635424852371216,
+//          "x":-2.6178488731384277,
+//          "z":4.162487983703613
+//       },
+//       "frame":1243884,
+//       "marker1":{
+//          "y":0.5650076866149902,
+//          "x":-2.7018988132476807,
+//          "z":4.124541759490967
+//       },
+//       "marker3":{
+//          "y":0.5635974407196045,
+//          "x":-2.6328799724578857,
+//          "z":4.239823341369629
+//       },
+//       "marker2":{
+//          "y":0.5645341873168945,
+//          "x":-2.6987645626068115,
+//          "z":4.241466522216797
+//       },
+//       "mocaptimestamp":34603.1719921,
+//       "position":{
+//          "y":0.5641704797744751,
+//          "x":-2.6628479957580566,
+//          "z":4.192079544067383
+//       },
+//       "rotation":{
+//          "y":-2.512839091650676e-05,
+//          "x":-6.7712971940636635e-06,
+//          "z":1.0973308235406876e-06,
+//          "w":-1.0
+//       },
+//       "id":3,
+//       "localtime":1511138724.6466603
+//    }
+// }
--- realtime/js/scene.js
+++ realtime/js/scene.js
@@ -23,13 +23,13 @@ initScene = function() {
 
     // Camera definition
     camera = new THREE.PerspectiveCamera(
-        25, // Field of view
-        window.innerWidth / window.innerHeight, // Aspect ratio
-        0.1, // Near
+        35, // Field of view
+        2,
+        0.5, // Near
         100 // Far
     );
-    camera.position.set(-3, 3, -2);
-    camera.lookAt(scene.position);
+    camera.position.set(5.5, 6, 4.2);
+    //camera.lookAt(scene.position);
     scene.add(camera);
 
     // Initialise lights
@@ -43,7 +43,7 @@ initScene = function() {
     });
     bulbLight.add(new THREE.Mesh(bulbGeometry, bulbMat));
     // The light is 2 meters minus 0.78 (the height of the table)
-    bulbLight.position.set(0, 1.22, 0);
+    bulbLight.position.set(-2, 2, 2);
     bulbLight.castShadow = true;
     scene.add(bulbLight);
 
@@ -73,53 +73,9 @@ initScene = function() {
     var floorMesh = new THREE.Mesh(floorGeometry, floorMat);
     floorMesh.receiveShadow = true;
     // The floor is minus 0.78 (the height of the table)
-    floorMesh.position.set(0, -0.78, 0); // position
+    floorMesh.position.set(-2, 0, 2.5); // position
     floorMesh.rotation.x = -Math.PI / 2.0;
     scene.add(floorMesh);
-
-    // Add app images
-    // marker1 (top right): 0.052582	0.030279	0.889596
-    // marker2 (bottom right): -0.474992	0.029005	0.888369
-    // marker3 (bottom left): -0.477954	0.027106	-0.051085
-    // marker4 (top left): 0.051298	0.026969	-0.049968
-    // middle top: 0.051, 0.028, 0.420
-    // middle bottom: -0.475, 0.028, 0.418
-    // screen centre: -0.212, 0.028, 0.419
-    // rotation: -0.000, -0.002, -0.000, -0.999
-    //img.position.set(-0.215, 0.01, 0.42); //initial position
-
-    // Black screen
-    img1.position.set(-0.212, 0, 0.419);
-    img1.rotation.y = 1.57;
-    //scene.add(img1);
-
-    // Empty flat screen
-    img2.position.set(-0.212, 0, 0.419);
-    img2.rotation.y = 1.57;
-
-    // Bathroom screen
-    img3.position.set(-0.212, 0, 0.419);
-    img3.rotation.y = 1.57;
-
-    // Bedroom screen
-    img4.position.set(-0.212, 0, 0.419);
-    img4.rotation.y = 1.57;
-
-    // Kitchen screen
-    img5.position.set(-0.212, 0, 0.419);
-    img5.rotation.y = 1.57;
-
-    // Living room screen
-    img6.position.set(-0.212, 0, 0.419);
-    img6.rotation.y = 1.57;
-
-    // Misc screen
-    img7.position.set(-0.212, 0, 0.419);
-    img7.rotation.y = 1.57;
-
-    // Rugs screen
-    img8.position.set(-0.212, 0, 0.419);
-    img8.rotation.y = 1.57;
 
     // Text 1
     text1.style.position = 'absolute';
@@ -128,54 +84,6 @@ initScene = function() {
     text1.style.backgroundColor = "yellow";
     text1.style.top = 10 + 'px';
     text1.style.left = 100 + 'px';
-
-    // Text 2
-    text2.style.position = 'absolute';
-    text2.style.width = 100;
-    text2.style.height = 100;
-    text2.style.backgroundColor = "yellow";
-    text2.style.top = 40 + 'px';
-    text2.style.left = 100 + 'px';
-
-    // Text 3
-    text3.style.position = 'absolute';
-    text3.style.width = 100;
-    text3.style.height = 100;
-    text3.style.backgroundColor = "yellow";
-    text3.style.top = 70 + 'px';
-    text3.style.left = 100 + 'px';
-
-    // Text 4
-    text4.style.position = 'absolute';
-    text4.style.width = 100;
-    text4.style.height = 100;
-    text4.style.backgroundColor = "yellow";
-    text4.style.top = 100 + 'px';
-    text4.style.left = 100 + 'px';
-
-    // Text 5
-    text5.style.position = 'absolute';
-    text5.style.width = 100;
-    text5.style.height = 100;
-    text5.style.backgroundColor = "white";
-    text5.style.top = 130 + 'px';
-    text5.style.left = 100 + 'px';
-
-    // Text 6
-    text6.style.position = 'absolute';
-    text6.style.width = 100;
-    text6.style.height = 100;
-    text6.style.backgroundColor = "white";
-    text6.style.top = 160 + 'px';
-    text6.style.left = 100 + 'px';
-
-    // Text 7
-    text7.style.position = 'absolute';
-    text7.style.width = 100;
-    text7.style.height = 100;
-    text7.style.backgroundColor = "white";
-    text7.style.top = 190 + 'px';
-    text7.style.left = 100 + 'px';
 
     // Glasses markers
     var dotGeometry = new THREE.Geometry();
@@ -191,37 +99,11 @@ initScene = function() {
     scene.add(g1marker4);
     g1markerc = new THREE.Points(dotGeometry, dotMaterial);
     scene.add(g1markerc);
-    g2marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g2marker1);
-    g2marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g2marker2);
-    g2marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g2marker3);
-    g2marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g2marker4);
-    g2markerc = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g2markerc);
-    g3marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g3marker1);
-    g3marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g3marker2);
-    g3marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g3marker3);
-    g3marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g3marker4);
-    g3markerc = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(g3markerc);
 
     // Glasses marker line
     var glinematerial1 = new THREE.LineBasicMaterial({
         color: 0xFFFFFF,
-        linewidth: 4 //this does not work at the moment
-    });
-    var glinematerial2 = new THREE.LineBasicMaterial({
-        color: 0x000000
-    });
-    var glinematerial3 = new THREE.LineBasicMaterial({
-        color: 0x0000ff
+        linewidth: 3
     });
     glinegeo1 = new THREE.Geometry();
     glinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
@@ -229,142 +111,8 @@ initScene = function() {
     glinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
     glinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
     glinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo2 = new THREE.Geometry();
-    glinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo3 = new THREE.Geometry();
-    glinegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
-    glinegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
     g1line = new THREE.Line(glinegeo1, glinematerial1);
     scene.add(g1line);
-    g2line = new THREE.Line(glinegeo2, glinematerial2);
-    scene.add(g2line);
-    g3line = new THREE.Line(glinegeo3, glinematerial3);
-    scene.add(g3line);
-
-    // Right Hand 1 markers
-    var dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
-    rh1marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh1marker1);
-    rh1marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh1marker2);
-    rh1marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh1marker3);
-    rh1marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh1marker4);
-
-    // Left Hand 1 markers
-    var dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
-    lh1marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh1marker1);
-    lh1marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh1marker2);
-    lh1marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh1marker3);
-    lh1marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh1marker4);
-
-    // Right Hand 2 markers
-    var dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
-    rh2marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh2marker1);
-    rh2marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh2marker2);
-    rh2marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh2marker3);
-    rh2marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(rh2marker4);
-
-    // Left Hand 2 markers
-    var dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
-    lh2marker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh2marker1);
-    lh2marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh2marker2);
-    lh2marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh2marker3);
-    lh2marker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(lh2marker4);
-
-    // Hands marker line
-    var hlinematerial1 = new THREE.LineBasicMaterial({
-        color: 0x000000
-    });
-    var hlinematerial2 = new THREE.LineBasicMaterial({
-        color: 0x0000ff
-    });
-    lh1linegeo = new THREE.Geometry();
-    lh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1linegeo = new THREE.Geometry();
-    rh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh2linegeo = new THREE.Geometry();
-    lh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    lh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh2linegeo = new THREE.Geometry();
-    rh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh2linegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    rh1line = new THREE.Line(rh1linegeo, hlinematerial1);
-    scene.add(rh1line);
-    lh1line = new THREE.Line(lh1linegeo, hlinematerial1);
-    scene.add(lh1line);
-    rh2line = new THREE.Line(rh2linegeo, hlinematerial2);
-    scene.add(rh2line);
-    lh2line = new THREE.Line(lh2linegeo, hlinematerial2);
-    scene.add(lh2line);
-
-    // Screen markers
-    var dotGeometry = new THREE.Geometry();
-    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
-    smarker1 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(smarker1);
-    smarker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(smarker2);
-    smarker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(smarker3);
-    smarker4 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(smarker4);
-
-    // Screen marker line
-    var slinematerial = new THREE.LineBasicMaterial({
-        color: 0xFFCC00
-    });
-    slinegeo = new THREE.Geometry();
-    slinegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    slinegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    slinegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    slinegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    slinegeo.vertices.push(new THREE.Vector3(0, 0, 0));
-    sline = new THREE.Line(slinegeo, slinematerial);
-    scene.add(sline);
 
     // GP3 markers
     var dotGeometry = new THREE.Geometry();
@@ -372,30 +120,75 @@ initScene = function() {
     var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
     gp3marker1 = new THREE.Points(dotGeometry, dotMaterial);
     scene.add(gp3marker1);
-    gp3marker2 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(gp3marker2);
-    gp3marker3 = new THREE.Points(dotGeometry, dotMaterial);
-    scene.add(gp3marker3);
 
     // GP3 marker line
     var gp3linematerial = new THREE.LineBasicMaterial({
-        color: 0xCC0000
+        color: 0xCC0000,
+        linewidth: 3
     });
     gp3linegeo1 = new THREE.Geometry();
     gp3linegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
     gp3linegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
-    gp3linegeo2 = new THREE.Geometry();
-    gp3linegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    gp3linegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
-    gp3linegeo3 = new THREE.Geometry();
-    gp3linegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
-    gp3linegeo3.vertices.push(new THREE.Vector3(0, 0, 0));
     gp3line1 = new THREE.Line(gp3linegeo1, gp3linematerial);
     scene.add(gp3line1);
-    gp3line2 = new THREE.Line(gp3linegeo2, gp3linematerial);
-    scene.add(gp3line2);
-    gp3line3 = new THREE.Line(gp3linegeo3, gp3linematerial);
-    scene.add(gp3line3);
+
+    // Target 1 markers
+    var dotGeometry = new THREE.Geometry();
+    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
+    t1marker1 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t1marker1);
+    t1marker2 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t1marker2);
+    t1marker3 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t1marker3);
+    t1marker4 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t1marker4);
+    t1markerc = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t1markerc);
+
+    // Target 1 marker line
+    var tlinematerial1 = new THREE.LineBasicMaterial({
+        color: 0x000000,
+        linewidth: 3
+    });
+    tlinegeo1 = new THREE.Geometry();
+    tlinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo1.vertices.push(new THREE.Vector3(0, 0, 0));
+    t1line = new THREE.Line(tlinegeo1, tlinematerial1);
+    scene.add(t1line);
+
+    // Target 2 markers
+    var dotGeometry = new THREE.Geometry();
+    dotGeometry.vertices.push(new THREE.Vector3(0, 0, 0));
+    var dotMaterial = new THREE.PointsMaterial({size: 5, sizeAttenuation: false, color: 0xFF3300});
+    t2marker1 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t2marker1);
+    t2marker2 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t2marker2);
+    t2marker3 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t2marker3);
+    t2marker4 = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t2marker4);
+    t2markerc = new THREE.Points(dotGeometry, dotMaterial);
+    scene.add(t2markerc);
+
+    // Target 2 marker line
+    var tlinematerial2 = new THREE.LineBasicMaterial({
+        color: 0x000000,
+        linewidth: 3
+    });
+    tlinegeo2 = new THREE.Geometry();
+    tlinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
+    tlinegeo2.vertices.push(new THREE.Vector3(0, 0, 0));
+    t2line = new THREE.Line(tlinegeo2, tlinematerial2);
+    scene.add(t2line);
 
     // Camera movement
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
@@ -413,18 +206,6 @@ initScene = function() {
     gui.add(params, 'exposure', 0, 1);
     gui.add(params, 'shadows');
     gui.open();
-
-    // Call the parsing function and the callback for position data
-    parsePositionData("data/recordings/rec12/output/rec12_data" + filevis + ".csv", positionDataHandler);
-
-    // Call the parsing function and the callback for ASR data
-    parseASRData("data/recordings/rec12/asr/rec12.csv", asrDataHandler);
-
-    // Call the parsing function and the callback for App data
-    parseAppData("data/recordings/rec12/app/rec12_final.csv", appDataHandler);
-
-    // Call the parsing function and the callback for Furniture data
-    parseFurnitureData("data/recordings/rec12/app/furniture.csv", furnitureDataHandler);
 
     requestAnimationFrame(render);
 };
